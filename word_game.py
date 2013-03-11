bd2f2a4eada5fb9acf1c8057067c8028f558524b
Kyle Morrison <fspoon64@gmail.com>
2013-03-10 23:40:21 -0700

    Added error for if input not a word.

--- word_game.py
+++ word_game.py
@@ -43,17 +43,21 @@ def bfs(node1, node2, dictionary):
     """
 
     dictionary_set = set(dictionary)
+
+    if node1 not in dictionary_set:
+        raise ValueError("'%s' not found in word list." % node1)
+    if node2 not in dictionary_set:
+        raise ValueError("'%s' not found in word list." % node2)
+
     queue = Queue.Queue()
-
     seen_already = set()
-
-    queue.put_nowait(node1)
-
     # Paths will be just next_node->prev_node, instead of maybe a more normal
     # next_node->(path_length, prev_node) for general pathfinding. This is
     # because it's BFS, so the first time we see a node, we know we've gotten
     # there optimally.
     paths = {}
+
+    queue.put_nowait(node1)
 
     while not queue.empty():
         next_node = queue.get_nowait()
@@ -70,7 +74,12 @@ def bfs(node1, node2, dictionary):
 
 if __name__ == '__main__':
     dictionary = read_word_list('word_list.txt')
-    path = bfs(sys.argv[1], sys.argv[2], dictionary)
-    if not path:
-        print "Couldn't find a path"
-    pprint.pprint(path)
+    try:
+        path = bfs(sys.argv[1], sys.argv[2], dictionary)
+        if not path:
+            print "Couldn't find a path"
+        pprint.pprint(path)
+    except ValueError, e:
+        print e
+        print "Please try again."
+
